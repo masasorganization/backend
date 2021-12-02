219f92fb180195c0def0c5421e5451c9b24281d3
lufermaxi <lufermaxi@gmail.com>
2021-12-02 17:05:01 -0500

    producto por categoria

--- src/routes/Producto.route.js
+++ src/routes/Producto.route.js
@@ -1,4 +1,5 @@
 const {Router} =require ('express')
+
 const router= Router()
 
 const ProductoCtrl = require('../controllers/Producto.controller')
@@ -6,13 +7,15 @@ const ProductoCtrl = require('../controllers/Producto.controller')
 const Auth= require ('../helper/Auth');
 const Auth2= require ('../helper/Auth2');
 
-router.post('/crear', [Auth2.verificarToken, Auth2.isAdmin],  ProductoCtrl.crear)
-router.get('/listarProductos', [Auth2.verificarToken, Auth2.isAdmin], ProductoCtrl.listar)
-router.put('/actualizar/:id', [Auth2.verificarToken, Auth2.isAdmin], ProductoCtrl.actualizar)
-router.delete('/eliminar/:id', [Auth2.verificarToken, Auth2.isAdmin], ProductoCtrl.eliminar) 
+router.post('/crear', ProductoCtrl.crear)
+router.get('/listarproductos', ProductoCtrl.listar)
+router.put('/actualizar/:id',  ProductoCtrl.actualizar)
+router.delete('/eliminar/:id', ProductoCtrl.eliminar) 
 /////---------------------------------------------------------------
-router.get('/listar/:id', ProductoCtrl.buscarPorId)
-router.get('/buscarpornombre/:nombre', [Auth2.verificarToken, Auth2.isAdmin], ProductoCtrl.buscarPorNombre)
-router.get('/buscarporcategoria/:categoria', [Auth2.verificarToken, Auth2.isAdmin], ProductoCtrl.buscarporCategoria)
+router.get('/buscarporid/:id', ProductoCtrl.buscarPorId)
+router.get('/buscarpornombre/:nombre', ProductoCtrl.buscarPorNombre)
+router.get('/buscarporcategoria/:categoria',  ProductoCtrl.buscarporCategoria)
+
+//[Auth2.verificarToken, Auth2.isAdmin],
 
 module.exports = router
